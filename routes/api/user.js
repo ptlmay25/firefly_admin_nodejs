1ed67aa1a10ccb3e6503e0c1ec8938925d759aa6
Vivek Shah <vivekshah9969@gmail.com>
2021-01-03 13:31:03 +0530

    user routes added for update and view

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,5 +2,9 @@ const router = require("express").Router();
 const UserController = require("../../controllers/user.controller");
 
 router.post("/create", UserController.create);
+router.post("/update", UserController.update);
+router.post("/view", UserController.view);
+router.post("/", UserController.viewAll);
+router.get("/", UserController.viewAll);
 
 module.exports = router;
