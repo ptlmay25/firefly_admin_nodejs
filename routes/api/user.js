287d46e1cdfac0abc0a676caaaa9cdfa6074bf3b
DevanshuBrahmbhatt <devanshu.vguj@gmail.com>
2021-01-08 09:15:34 +0530

    check user by mobile phone is completed some fixes also completed

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,7 +4,7 @@ const UserController = require("../../controllers/user.controller");
 router.post("/create", UserController.create);
 router.put("/update", UserController.update);
 router.get("/view/:userId", UserController.view);
-router.get("/view/:mobileNo", UserController.viewWithMobileNO);
+router.get("/viewMobile/:mobileNo", UserController.viewWithMobileNO);
 router.get("/", UserController.viewAll);
 router.get("/checkUser/:mobileNo", UserController.userCheck)
 
