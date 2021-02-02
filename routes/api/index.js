ad1eaaaa46e0bae1e4ea0624feaa1e2761353404
DevanshuBrahmbhatt <devanshu.vguj@gmail.com>
2021-02-02 21:41:30 +0530

    Withdraw Request CRUD API done

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const user = require('./user');
 const tokenHistory = require('./tokenhistory');
 const withdrawHistory = require('./withdrawHistory');
+const withdrawRequest = require('./withdrawrequest');
 
 router.use('/user', user);
 router.use('/token', tokenHistory);
-router.use('/withdrawHistory', withdrawHistory)
+router.use('/withdrawHistory', withdrawHistory);
+router.use('/withdrawRequest', withdrawRequest);
 
 
 module.exports = router;
