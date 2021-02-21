d9160834a680ccc3d99e97735ad80ae3475f4255
Vivek Shah <vivekshah9969@gmail.com>
2021-02-22 00:31:10 +0530

    token history all changes done

--- controllers/tokenhistory.controller.js
+++ controllers/tokenhistory.controller.js
@@ -1,9 +1,11 @@
 const Afterware = require("../lib/afterware");
-const token = require("../models/token");
 const Collection = require("../models/token");
+const UserCollection = require("../models/user");
 const UserController = require("./user.controller");
 
 class TokenHistoryController {
+
+    static DEFAULT_PRICE = 10000;
 
     static async create(req, res) {
         try {
@@ -20,7 +22,7 @@ class TokenHistoryController {
                 });
             }
             
-            for(key in ["total_revenue","operating_expenses","interest_and_taxes","service_fee","net_profit"])
+            for(let key in ["total_revenue","operating_expenses","interest_and_taxes","service_fee","net_profit"])
             {
                 tokenHistory[key] = parseInt(tokenHistory[key],10)
                 if(tokenHistory[key] == NaN){
@@ -30,11 +32,11 @@ class TokenHistoryController {
 
             // validating
             if(
-                tokenHistory["total_revenue"]<=0 ||
-                tokenHistory["operating_expenses"]<=0 ||
-                tokenHistory["interest_and_taxes"]<=0 ||
-                tokenHistory["service_fee"]<=0 ||
-                tokenHistory["net_profit"]<=0 ||
+                tokenHistory["total_revenue"]<0 ||
+                tokenHistory["operating_expenses"]<0 ||
+                tokenHistory["interest_and_taxes"]<0 ||
+                tokenHistory["service_fee"]<0 ||
+                tokenHistory["net_profit"]<0 ||
                 tokenHistory["service_fee"] != SERVICE_FEE*tokenHistory["total_revenue"] || 
                 tokenHistory["net_profit"] != (tokenHistory["total_revenue"] - tokenHistory["operating_expenses"] - tokenHistory["interest_and_taxes"] - tokenHistory["service_fee"])
             )
@@ -45,14 +47,21 @@ class TokenHistoryController {
                 });
             }
 
-            const token_price = TokenHistoryController.getLatestTokenPrice();
-            const total_number_of_tokens = TokenHistoryController.getTotalNumberOfTokens();
+            const total_number_of_tokens = await TokenHistoryController._getNumberOfTokens() || 1;
+            const token_price = (await TokenHistoryController._getLatestTokenPrice()).token_price
 
+            let divident = (tokenHistory["net_profit"] || 0)/total_number_of_tokens;
+            let new_token_price = token_price + divident
             
+            tokenHistory["total_number_of_tokens"] = total_number_of_tokens;
+            tokenHistory["token_price"] = new_token_price;
+
             const collection = new Collection(tokenHistory);
-            
+            const savedDoc = await collection.save()
+
             return Afterware.sendResponse(req, res, 200, {
                 status: "success",
+                data: savedDoc,
                 message: "new token history collection created successfully",
             });
         } catch (error) {
@@ -61,6 +70,30 @@ class TokenHistoryController {
                 status: "error",
                 message: "Internal Server Error",
                 messageDetails: error
+            });
+        }
+    }
+
+    static async view(req, res) {
+        try {
+            const tokenId = req.params.id;
+            if (!tokenId || tokenId === "") {
+                return Afterware.sendResponse(req, res, 400, {
+                    status: "Validation Error",
+                    message: "Enter Proper inputs",
+                });
+            } else {
+                const collections = await Collection.find({ _id: tokenId });
+                return Afterware.sendResponse(req, res, 200, {
+                    status: "success",
+                    data: collections,
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return Afterware.sendResponse(req, res, 500, {
+                status: "error",
+                message: "Internal Server Error",
             });
         }
     }
@@ -82,22 +115,121 @@ class TokenHistoryController {
     }
 
     static async delete(req,res){
-
+        const tokenId = req.params.id;
+        try {
+            const deleted = await Collection.deleteOne({ _id: tokenId });
+            return Afterware.sendResponse(req, res, 200, {
+                status: deleted.ok == "1" ? "success" : "fail",
+                message: deleted.deletedCount,
+            });
+        } catch (error) {
+            return Afterware.sendResponse(req, res, 500, {
+                status: "error",
+                message: "Internal Server Error",
+            });
+        }
     }
 
     static async update(req,res){
-
+        try {
+            const userId = req.params.id;
+            if (!userId && userId === "") {
+                return Afterware.sendResponse(req, res, 400, {
+                    status: "Validation Error",
+                    message: "Enter Proper input",
+                });
+            } else {
+                const updated = await Collection.updateOne({ _id: userId }, req.body);
+                return Afterware.sendResponse(req, res, 200, {
+                    status: "success",
+                    message: `${updated.nModified} Documents modified`,
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            return Afterware.sendResponse(req, res, 500, {
+                status: "error",
+                message: "Internal Server Error",
+            });
+        }
     }
 
     static async getLatestTokenPrice(req,res){
-
+        try{
+            let latestTokenPrice = await TokenHistoryController._getLatestTokenPrice();
+            return Afterware.sendResponse(req, res, 200, {
+                status: "success",
+                data: latestTokenPrice,
+            });
+        }
+        catch(error){
+            console.log(error)
+            return Afterware.sendResponse(req, res, 500, {
+                status: "error",
+                message: "Internal Server Error",
+            });
+        }
     }
 
     static async validate(data){
         const requiredFields = ["total_revenue","operating_expenses","interest_and_taxes","service_fee","net_profit"]
         const validated = requiredFields.every(key=>key in data)
-        console.log(validated)
         return validated
+    }
+
+    static async _getLatestTokenPrice(){
+        let latestPrice = {
+            token_date: null,
+            token_price: TokenHistoryController.DEFAULT_PRICE
+        }
+        try{
+            const latestPriceArray =  await Collection.find().sort({token_date: -1, token_price: -1}).limit(1);
+            if(latestPriceArray.length==1)
+            {
+                latestPrice = latestPriceArray[0];
+            }
+            if(latestPrice.token_price==undefined || latestPrice.token_price==0)
+            {
+                latestPrice.token_price = TokenHistoryController.DEFAULT_PRICE;
+            }
+            return latestPrice
+        }
+        catch(error){
+            console.log(error)
+            return {
+                token_date: null,
+                token_price: TokenHistoryController.DEFAULT_PRICE
+            }
+        };
+    }
+    
+    static async getNumberOfTokens(req,res){
+        try{
+            let NumberOfTokens = await TokenHistoryController._getNumberOfTokens()
+            return Afterware.sendResponse(req, res, 200, {
+                status: "success",
+                data: NumberOfTokens,
+            });
+        }
+        catch(error){
+            console.log(error)
+            return Afterware.sendResponse(req, res, 500, {
+                status: "error",
+                message: "Internal Server Error",
+            });
+        }
+    }
+
+    static async _getNumberOfTokens(){
+        try{
+            let NumberOfTokens = await UserCollection.aggregate([{ $group: { _id: null, tokens: { $sum: "$tokens" }}}]);
+            NumberOfTokens = NumberOfTokens[0].tokens
+            return NumberOfTokens || 0; 
+        }
+        catch(error){
+            console.log(error)
+            return 0;
+        }
     }
 }
 
--- routes/api/token.js
+++ routes/api/token.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const Controller = require("../../controllers/tokenhistory.controller");
 
 router.get("/", Controller.viewAll);
+router.get("/view/:id", Controller.view)
 router.get("/getLatestTokenPrice", Controller.getLatestTokenPrice);
+router.get("/getNumberOfTokens", Controller.getNumberOfTokens);
 router.post("/create", Controller.create);
 router.put("/update/:id", Controller.update);
 router.delete("/delete/:id", Controller.delete);
