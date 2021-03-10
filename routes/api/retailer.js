66a35716105b9a18e61c075d11ed619cbbfe3ff5
Manan Dalal <manandalal1012@gmail.com>
2021-03-10 16:19:24 +0530

    retailer route fix

--- routes/api/retailer.js
+++ routes/api/retailer.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const RetailerController = require("../../controllers/retailer.controller");
 
-router.post("/create", UserController.create);
-router.get("/", UserController.viewAll);
-router.delete("/delete/:mobileNo", UserController.delete) //It should be done with _id only
+router.post("/create", RetailerController.create);
+router.get("/", RetailerController.viewAll);
+router.delete("/delete/:mobileNo", RetailerController.delete) //It should be done with _id only
 
 module.exports = router;
